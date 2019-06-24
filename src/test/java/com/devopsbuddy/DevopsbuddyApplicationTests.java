1acac10a0984595ac5d1f037e365b4defb2714a6
Grimripper <gyovannycavazosmarquez@gmail.com>
2019-06-23 17:06:07 -0700

    added test and translations base

--- src/test/java/com/devopsbuddy/DevopsbuddyApplicationTests.java
+++ src/test/java/com/devopsbuddy/DevopsbuddyApplicationTests.java
@@ -1,13 +1,29 @@
 package com.devopsbuddy;
 
+import com.devopsbuddy.web.i18n.I18NService;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
+
+@RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
+@WebAppConfiguration
 class DevopsbuddyApplicationTests {
 
+    @Autowired
+    private I18NService i18NService;
+
     @Test
-    void contextLoads() {
+    public void testMessageByLocaleService(){
+        String expectedResult = "Mi Bootstrap thymeleaf page";
+        String messageId = "index.main.callout";
+        String actual = i18NService.getMessage(messageId);
+        Assert.assertEquals("The actual and expected don't match", expectedResult, actual);
     }
 
 }
