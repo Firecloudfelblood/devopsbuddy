e87fb3804a88c5510114124b6b08c582a91f3d21
Grimripper <gyovannycavazosmarquez@gmail.com>
2019-06-30 19:00:44 -0700

    added test and translations base

---
+++ src/main/resources/static/js/devopsbuddy.js
@@ -0,0 +1,11 @@
+// $(document).ready(()=>{
+//     $(".btn-collapse").click((e)=> {
+//         e.preventDefault();
+//         var $this = $(this);
+//         var $collapse = $this.closest('.collapse-group').find('.collapse');
+//         $collapse.collapse('toggle');
+//     });
+// });
+
+
+
