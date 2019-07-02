9b37447fba9645f5836da9105a393ee5da044960
Grimripper <gyovannycavazosmarquez@gmail.com>
2019-07-01 20:35:09 -0700

    added test and translations base

---
+++ src/main/java/com/devopsbuddy/backend/service/MockEmailService.java
@@ -0,0 +1,11 @@
+package com.devopsbuddy.backend.service;
+
+import org.springframework.mail.SimpleMailMessage;
+
+public class MockEmailService {
+
+    @Override
+    public void sendGenericEmailMessage(SimpleMailMessage message){
+
+    }
+}
--- src/main/java/com/devopsbuddy/web/controllers/ContactController.java
+++ src/main/java/com/devopsbuddy/web/controllers/ContactController.java
@@ -1,14 +1,17 @@
 package com.devopsbuddy.web.controllers;
 
 import com.devopsbuddy.web.domain.frontend.FeedbackPojo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class ContactController {
-
+    public static final Logger LOG = LoggerFactory.getLogger(ContactController.class);
     public static final  String FEEDBACK_MODEL_KEY = "feedback";
     public static final  String CONTACT_US_VIEW_NAME = "contact/contact";
 
@@ -18,4 +21,13 @@ public class ContactController {
         model.addAttribute(ContactController.FEEDBACK_MODEL_KEY, feedbackPojo);
         return ContactController.CONTACT_US_VIEW_NAME;
     }
+
+    @RequestMapping(value ="/contact", method= RequestMethod.POST)
+    public String contactPost(@ModelAttribute(FEEDBACK_MODEL_KEY)FeedbackPojo feedback){
+        LOG.debug("Feedback from the pojo content: {}", feedback);
+
+        return ContactController.CONTACT_US_VIEW_NAME;
+    }
+
+
 }
